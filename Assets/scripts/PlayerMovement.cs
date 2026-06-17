348eb1af1410a8ccb4f1aa97e1f087d8ebcd844c
siddharth-0708 <siddharth0708@gmail.com>
2026-06-17 08:38:25 +0530

    changes

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -16,15 +16,23 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        if (IsAttacking()){
+        UpdateFacing();
+
+        if (IsAttacking())
             return;
-        }
+
         Vector3 move = new Vector3(moveInput.x, 0f, 0f);
-        transform.Translate(move * moveSpeed * Time.deltaTime, Space.World);
+        transform.Translate(-move * moveSpeed * Time.deltaTime, Space.World);
+    }
+
+    void UpdateFacing()
+    {
+        float yRotation = moveInput.x < 0f ? 90f : -90f;
+        transform.rotation = Quaternion.Euler(0f, yRotation, 0f);
     }
     bool IsAttacking()
-{
-    if (animator.IsInTransition(0)) return true;
+    {
+        if (animator.IsInTransition(0)) return true;
         var state = animator.GetCurrentAnimatorStateInfo(0);
         return state.IsName("punch1") || state.IsName("punch2")
             || state.IsName("kick1") || state.IsName("kick2");
