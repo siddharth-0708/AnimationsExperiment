543821670168d5214b4426456aaf0d7e1179fe56
siddharth-0708 <siddharth0708@gmail.com>
2026-06-20 20:26:15 +0530

    changes

--- Assets/scripts/CollisionHandler.cs
+++ Assets/scripts/CollisionHandler.cs
@@ -6,8 +6,16 @@ public class CollisionHandler : MonoBehaviour
     {
         Debug.Log("Collision detected with: " + other.gameObject.name);
         if (!other.gameObject.CompareTag("tripleh"))
+        {
             return;
-
+        }
+        Collider ourHitbox = other.GetContact(0).thisCollider;
+        GameObject hitter = ourHitbox.gameObject;
+        
+        if (!hitter.CompareTag("hitBox"))
+        {
+            return;
+        }
         Animator enemyAnimator = other.gameObject.GetComponent<Animator>();
         if (enemyAnimator == null)
             return;
